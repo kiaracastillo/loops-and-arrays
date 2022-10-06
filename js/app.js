1bb2f8e900b17ed34144fe8bef00ed4d3239cbb5
Kiara Castillo <kiaracastillom@gmail.com>
2022-10-06 17:56:37 -0400

    final

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/*
+Kiara Castillo Magallanes
+10/06/2022
+Loops and arrays
+*/
+
+let numeros = document.getElementById("numeros"); //id property matches
+
+
+for( i = 1000; i < 5001; i += 1000) {
+    numeros.innerHTML+=i + " ";
+}
+
